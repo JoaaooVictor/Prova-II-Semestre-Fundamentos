ca733f6bd6375eec6d6e9a75e7fa040e0c686e43
João Victor <joao.adm.etec@outlook.com>
2022-07-06 00:19:41 -0300

    Adicionar arquivos de projeto.

---
+++ Prova(II Bimestre)/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prova_II_Bimestre_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] matricula = new string[5];
+            string[] nome = new string[5];
+            double[] sal_a = new double[5];
+            double[] salario = new double[5];
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("-------------------------");
+                Console.Write("Olá, Digite sua matricula: ");
+                matricula[i] = Console.ReadLine();
+                Console.Write("Digite seu nome: ");
+                nome[i] = Console.ReadLine();
+                Console.Write("Digite seu salário atual: ");
+                salario[i] = int.Parse(Console.ReadLine());
+            }
+            
+            for (int i = 0; i < 5; i++)
+            {
+                
+                if (salario[i] >= 8000)
+                {
+                    sal_a[i] = salario[i] * 1.025;
+                }
+                else if (salario[i] < 8000)
+                {
+                    sal_a[i] = salario[i] * 1.035;
+                }
+                
+                Console.WriteLine("--------------------------");
+                Console.WriteLine("Nome: " + nome[i]);
+                Console.WriteLine("Numero de Matricula: " + matricula[i]);
+                Console.WriteLine("Esse é seu salário antigo: " + salario[i]);
+                Console.WriteLine("Este é seu salário atual: " + sal_a[i]);
+                
+            }
+                Console.ReadKey();
+        }
+    }
+}
